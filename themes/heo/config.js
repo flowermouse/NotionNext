9d22c84996fa8cacb689769380d23010483de2d2
flowermouse <92005952+flowermouse@users.noreply.github.com>
2025-03-02 15:23:19 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,9 +18,9 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '行公义',
-  HEO_HERO_TITLE_2: '好怜悯',
-  HEO_HERO_TITLE_3: '存谦卑的心与你的神同行',
+  HEO_HERO_TITLE_1: '行公义，好怜悯',
+  HEO_HERO_TITLE_2: '存谦卑的心，与你的神同行',
+  HEO_HERO_TITLE_3: 'To act justly and to love mercy and to walk humbly with your God',
   HEO_HERO_TITLE_4: '行走大地',
   HEO_HERO_TITLE_5: '路漫漫其修远兮，吾将上下而求索',
   HEO_HERO_TITLE_LINK: 'https://github.com/flowermouse',
@@ -29,7 +29,7 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_2: { title: '推荐文章', url: '/tag/热门文章' },
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
